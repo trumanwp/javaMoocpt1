0f41dfb49f1e42bd8703fba788b8bf69753d272c
Adam <adambowieday3@gmail.com>
2025-03-26 18:45:32 +0000

    Initial commit

---
+++ part02-Part02_32.Averaging/src/main/java/Averaging.java
@@ -0,0 +1,22 @@
+
+public class Averaging {
+
+    // implement the sum method here again
+    public static int sum(int number1, int number2, int number3, int number4) {
+        // write some code here
+        int result = number1 + number2 + number3 + number4;
+        return result;
+    }
+
+    public static double average(int number1, int number2, int number3, int number4) {
+        // write some code here
+        double avg = (double) (sum(number1, number2, number3, number4)) / 4;
+        return avg;
+
+    }
+
+    public static void main(String[] args) {
+        double result = average(4, 3, 6, 1);
+        System.out.println("Average: " + result);
+    }
+}
